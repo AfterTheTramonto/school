b988453cc6ff13f33c5359bcb8e0232859ea76af
denis <denisik33@yandex.ru>
2025-10-19 19:30:21 +0300

    HM

---
+++ src/main/java/ru/hogwarts/school/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package ru.hogwarts.school.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hogwarts School API",
+                description = "API для управления студентами и факультетами школы Хогвартс",
+                version = "1.0.0"
+        )
+)
+public class OpenApiConfig {
+}
